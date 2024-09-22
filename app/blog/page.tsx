2b27e72e0031cb0bf357dbd988d6ab2b8ad6b7b2
Marcos Lucas <mac@Macs-MacBook-Pro.local>
2024-09-22 18:39:54 -0300

    ajuste para build

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "next/navigation";
 import useFetchPosts from "../../hooks/useFetchPosts";
 import PostCard from "@/components/PostCard";
 import Loading from "@/components/Loading";
+
 function BlogPage() {
   const searchParams = useSearchParams();
   const keyword = searchParams.get("keyword") || "";
@@ -14,12 +15,13 @@ function BlogPage() {
 
   return (
     <div className="pt-28 mx-5 sm:m-0">
-      <h1 className="text-3xl sm:text-4xl font-semibold text-secondary weight-600">
+      <h1 className="text-3xl sm:text-4xl font-semibold text-secondary">
         Resultados da Busca
       </h1>
       {keyword && (
         <h2>
-          Palavra-chave: <span className="text-primary">"{keyword}"</span>
+          Palavra-chave:{" "}
+          <span className="text-primary">&quot;{keyword}&quot;</span>
         </h2>
       )}
       {categoryId > 0 && <h2>ID da Categoria: {categoryId}</h2>}
@@ -29,18 +31,22 @@ function BlogPage() {
           <Loading />
         </div>
       )}
-      {error && <p>{error}</p>}
+      {error && <p className="text-red-500">{error}</p>}
 
       <div className="flex my-5 flex-wrap">
-        {posts.map((post) => (
-          <div key={post.id} className="w-1/2 sm:w-1/3 lg:w-1/4 ">
-            <PostCard
-              title={post.title.rendered}
-              imageUrl={post._embedded?.["wp:featuredmedia"]?.[0]?.source_url}
-              link={post.link}
-            />
-          </div>
-        ))}
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <div key={post.id} className="w-1/2 sm:w-1/3 lg:w-1/4">
+              <PostCard
+                title={post.title.rendered}
+                imageUrl={post._embedded?.["wp:featuredmedia"]?.[0]?.source_url}
+                link={post.link}
+              />
+            </div>
+          ))
+        ) : (
+          <p>Nenhum post encontrado.</p>
+        )}
       </div>
     </div>
   );
