3cdcf79e3b09355ada95b790ade1de35c20f8295
Marcos Lucas <mac@Macs-MacBook-Pro.local>
2024-09-22 18:59:17 -0300

    correcao do build

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import useFetchPosts from "../../hooks/useFetchPosts";
 import PostCard from "@/components/PostCard";
@@ -20,9 +19,11 @@ function BlogPage() {
       </h1>
       {keyword && (
         <h2>
-          Palavra-chave: <span className="text-primary">{keyword}</span>
+          Palavra-chave:{" "}
+          <span className="text-primary">&quot;{keyword}&quot;</span>
         </h2>
       )}
+      {categoryId > 0 && <h2>ID da Categoria: {categoryId}</h2>}
 
       {loading && (
         <div>
@@ -31,23 +32,25 @@ function BlogPage() {
       )}
       {error && <p>{error}</p>}
 
-      {Array.isArray(posts) && posts.length > 0 ? (
-        <div className="flex my-5 flex-wrap">
-          {posts.map((post) => (
-            <div key={post.id} className="w-1/2 sm:w-1/3 lg:w-1/4 ">
-              <PostCard
-                title={post.title.rendered}
-                imageUrl={post._embedded?.["wp:featuredmedia"]?.[0]?.source_url}
-                link={post.link}
-              />
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Nenhum resultado encontrado</p>
-      )}
+      <div className="flex my-5 flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id} className="w-1/2 sm:w-1/3 lg:w-1/4 ">
+            <PostCard
+              title={post.title.rendered}
+              imageUrl={post._embedded?.["wp:featuredmedia"]?.[0]?.source_url}
+              link={post.link}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default BlogPage;
+export default function BlogWrapper() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <BlogPage />
+    </Suspense>
+  );
+}
